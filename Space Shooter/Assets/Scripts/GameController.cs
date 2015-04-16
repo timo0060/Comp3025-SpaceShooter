b4c77843468edcf580fb48a7b3bbf6d7bf463c3b
michealpwalls <michealpwalls@gmail.com>
2015-04-16 15:39:27 -0400

    Add Infinite loop of hazard creations [Waves]

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -4,18 +4,35 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 	public GameObject hazard;
 	public Vector3 spawnValue;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	//Instantiate hazards for the player
-	void SpawnWaves() {
-		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
-		Quaternion spawnRotation = Quaternion.identity;
+	IEnumerator SpawnWaves() {
+		//Wait
+		yield return new WaitForSeconds(startWait);
 
-		Instantiate (hazard, spawnPosition, spawnRotation);
-	}
+		while(true) {
+			for(int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+				Quaternion spawnRotation = Quaternion.identity;
+
+				Instantiate (hazard, spawnPosition, spawnRotation);
+
+				//Wait
+				yield return new WaitForSeconds(spawnWait);
+			}//end of for loop
+
+			yield return new WaitForSeconds(waveWait);
+		}//end of while loop
+
+	}//end of SpawnWaves method
 
 	// Use this for initialization
 	void Start () {
-		SpawnWaves ();
+		StartCoroutine(SpawnWaves());
 	}
 	
 	// Update is called once per frame
