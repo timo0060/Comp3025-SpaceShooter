16f4eee75856ffbaaa6ea05efea1e901f846547d
michealpwalls <michealpwalls@gmail.com>
2015-04-16 15:18:47 -0400

    Add basic GameController [GameController]

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject hazard;
+	public Vector3 spawnValue;
+
+	//Instantiate hazards for the player
+	void SpawnWaves() {
+		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+		Quaternion spawnRotation = Quaternion.identity;
+
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpawnWaves ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
