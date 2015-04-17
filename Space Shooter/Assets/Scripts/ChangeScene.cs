53a3e662e5fd80023271e4373b6b557972602766
timo0060 <timo0060@gmail.com>
2015-04-17 01:18:54 -0400

    Added main menu and game over screen

---
+++ Space Shooter/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public void ChangeToScene(string sceneToChange)
+    {
+        Application.LoadLevel(sceneToChange);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
